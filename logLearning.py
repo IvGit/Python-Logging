09dd74a27f100f87760cdfa5db1d5d863ac70544
Barxemen lolo <Kareresh100@outlook.com>
2022-06-03 22:05:25 +0200

    working math file

---
+++ logLearning.py
@@ -0,0 +1,27 @@
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def Multipy(x,y):
+    return x*y
+
+def Devide(x,y):
+    return x/y
+
+num_1 =10
+num_2 = 5
+
+add_re = add(num_1,num_2)
+print('Add: {} + {} = {}'.format(num_1, num_2, add_re))
+
+sub_re = subtract(num_1,num_2)
+print('Sub: {} + {} = {}'.format(num_1, num_2, sub_re))
+
+multi_re = Multipy(num_1,num_2)
+print('Multi: {} + {} = {}'.format(num_1,num_2,multi_re))
+
+devide_re = Devide(num_1,num_2)
+print('Dvi: {} + {} = {}'.format(num_1,num_2,devide_re))
